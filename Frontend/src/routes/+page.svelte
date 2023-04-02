71042604f36cdf6ab556b6f84cfeb908f6fcb3d8
GyörgyFerenc <gyferenc2002@gmail.com>
2023-04-02 17:44:53 +0300

    Added CORS

---
+++ Frontend/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from "svelte";
+
+    let alma = "alma";
+
+    async function getPersons() {
+        let p = await fetch(
+            "https://flask-api-381710.lm.r.appspot.com/person/"
+        );
+
+        return p.json();
+    }
+
+    let persons = "";
+
+    onMount(async () => {
+        let j = await getPersons();
+        persons = j.toString();
+        console.log(persons);
+    });
+</script>
+
+<section>
+    asd {persons}
+</section>
