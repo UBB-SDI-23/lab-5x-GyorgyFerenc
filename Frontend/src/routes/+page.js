c4a6c0478afdd307d2ae0c19e503dffa378ac4ea
GyörgyFerenc <gyferenc2002@gmail.com>
2023-04-10 08:38:52 +0300

    New try

--- Frontend/src/routes/+page.js
+++ Frontend/src/routes/+page.js
@@ -1,11 +1,11 @@
-import { getPersons } from "./backend.js";
+import { getPersons, getPerson } from "./backend.js";
 
 export const ssr = false;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
     let data = {
-        persons: getPersons(),
+        persons: [getPerson(2)],
     };
 
     return data;
